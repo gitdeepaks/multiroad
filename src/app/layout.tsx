8defcd4698c22124826ea5888908bfeddd0c05eb
gitdeepaks <deepaksankhyan92@outlook.com>
2025-04-15 21:58:14 +0530

    01-starting project

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { DM_Sans } from "next/font/google";
+
+import "./globals.css";
+
+const dmSans = DM_Sans({
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Multi-Tanenet-E-Commerce",
+  description: "Multi-Tanenet-E-Commerce",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${dmSans.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
